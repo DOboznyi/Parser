aca2687841ec9dafb68a0ba0df1e2537ec84ffc8
Обозный Дмитрий <DObozniy@gmail.com>
2018-10-26 20:11:39 +0300

    auto tag in assemblyinfo.cs added

--- Parser/Parser/Program.cs
+++ Parser/Parser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,10 @@ namespace Parser
     {
         static void Main(string[] args)
         {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.ProductVersion;
+            Console.WriteLine("Program version "+version);
             Console.WriteLine("Hello in zakon parcer. You need to enter the path to you *.htm law. Please note, I can parce laws only from http://zakon.rada.gov.ua/laws/file/2341-14. Edition must be from 2015 till now.");
             Console.WriteLine("Your path with *.htm file: ");
             string path = @Console.ReadLine();
